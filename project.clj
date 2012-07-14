b9390427a46bb6d088558a12e17d48ce809cc319
Jon Distad <jon.distad@gmail.com>
2012-07-14 15:47:13 -0400

    Let's start without datomic and see if I can figure out what the hell I'm trying to do.

--- project.clj
+++ project.clj
@@ -1,5 +1,4 @@
 (defproject rule-engine "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [com.datomic/datomic "0.1.3164"]]
+  :dependencies [[org.clojure/clojure "1.4.0"]]
   :plugins [[lein-swank "1.4.4"]])
