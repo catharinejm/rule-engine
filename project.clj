a43cf5869bcdd036e797be548e912a78a020c7e1
Jon Distad <jon.distad@gmail.com>
2012-07-14 18:46:22 -0400

    Basic transitions

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject rule-engine "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.4.0"]]
-  :plugins [[lein-swank "1.4.4"]])
+  :plugins [[lein-swank "1.4.4"]]
+  :main rule-engine.core)
--- src/rule_engine/core.clj
+++ src/rule_engine/core.clj
@@ -1,1 +1,63 @@
 (ns rule-engine.core)
+
+(defrecord Transition [predicate next-step])
+(defrecord Step [name transitions required-values])
+
+(def patient (atom {}))
+(def coercers
+  (letfn [(to-num [n]
+            (try (Long/parseLong n)
+                 (catch NumberFormatException e
+                   nil)))]
+    {:age to-num}))
+
+(defn update-patient! [new-map]
+  (let [coerce-fn (fn [k] (get coercers k identity))
+        coerced-map (reduce (fn [map [k v]]
+                              (assoc map k ((coerce-fn k) v)))
+                            {} new-map)]
+    (swap! patient #(merge % coerced-map))))
+
+(def root-step (Step. "Root"
+                      [(Transition. #(> (:age @patient) 42)
+                                    (map->Step {:name "Is old"}))
+                       (Transition. (constantly true)
+                                    (map->Step {:name "Is young"}))]
+                      #{:age}))
+
+(defn children [step]
+  (into [] (map :next-step (:transitions step))))
+
+(defn final? [step] (empty? (:transitions step)))
+
+(defn missing-attributes [step]
+  (let [ptnt-map @patient]
+    (filter #(nil? (ptnt-map %)) (:required-values step))))
+
+(defn ready-to-transition? [step]
+  (empty? (missing-attributes step)))
+
+(defn next-step [step]
+  (let [missing (missing-attributes step)]
+    (if (empty? missing)
+      (some #(when ((:predicate %)) (:next-step %))
+            (:transitions step))
+      (assoc step :missing-attributes missing))))
+
+(defn get-attributes! [attrs]
+  (update-patient! (reduce
+                    (fn [map key]
+                      (println "Enter" (name key))
+                      (assoc map key (read-line)))
+                    {} attrs)))
+
+(defn run! []
+  (loop [step root-step]
+    (if (final? step)
+      step
+      (let [nxt (next-step step)]
+        (when (contains? nxt :missing-attributes)
+          (get-attributes! (:missing-attributes nxt)))
+        (recur nxt)))))
+
+(defn -main [] (run!))
